90053917d08762fe27a3c7fa67061155a984eaf9
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-22 19:42:52 +0300

    Added serializers.py for user

--- user/serializers.py
+++ user/serializers.py
@@ -1,12 +1,33 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserSerializer(serializers.ModelSerializer):
+    follower = serializers.StringRelatedField(many=True, read_only=True)
+    following = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
+        fields = (
+            "id",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+            "bio",
+            "avatar",
+            "follower",
+            "following",
+        )
+        read_only_fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "is_staff",
+            "avatar",
+            "bio",
+        )
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
@@ -22,3 +43,29 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class TokenSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    def validate(self, attrs):
+        refresh = attrs["refresh"]
+
+        try:
+            token = RefreshToken(refresh)
+            token.blacklist()
+        except Exception:
+            raise serializers.ValidationError("Token is invalid or expired")
+
+        return attrs
+
+
+class PartialUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "first_name",
+            "last_name",
+            "bio",
+            "avatar",
+        )
