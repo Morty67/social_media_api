d30d9f17f0f967dec9aecceaac23fb507bbbeb7d
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-22 19:44:37 +0300

    Added new views for user

--- user/views.py
+++ user/views.py
@@ -1,8 +1,15 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status, permissions
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    TokenSerializer,
+    PartialUserSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -16,3 +23,44 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class TokenLogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PartialUserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = PartialUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(id=self.request.user.id)
+
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = get_user_model().objects.all()
+        email = self.request.query_params.get("email")
+        first_name = self.request.query_params.get("first_name")
+        if email:
+            queryset = queryset.filter(email__icontains=email)
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        return queryset
