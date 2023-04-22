86a388954a11e7ed568ad513c220e82fb2b7f283
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-22 19:35:08 +0300

    Changes in models for user and another models

--- social_media_api/models.py
+++ social_media_api/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from user.models import User
 
@@ -6,14 +7,23 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    media = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.content
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -21,8 +31,16 @@ class Like(models.Model):
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -32,13 +50,16 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     follower = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="follower",
         on_delete=models.CASCADE,
     )
     following = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="following",
         on_delete=models.CASCADE,
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Follower - ({self.follower}) - Following ({self.following})"
--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
+import os
+from django.utils.text import slugify
+import uuid
+
 from django.contrib.auth.models import (
     AbstractUser,
     BaseUserManager,
@@ -43,8 +47,24 @@ class UserManager(BaseUserManager):
 class User(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        return self.first_name
