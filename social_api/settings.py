489c7a80b37fad5c7bed6549effb820c84cdda29
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-22 19:33:47 +0300

    Changes in settings.py

--- social_api/settings.py
+++ social_api/settings.py
@@ -59,8 +59,7 @@ ROOT_URLCONF = "social_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,6 +122,8 @@ AUTH_USER_MODEL = "user.User"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
