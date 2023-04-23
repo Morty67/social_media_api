55ed423c6d3e41269b7841c4db5683606ffde4c7
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-04-23 12:15:45 +0300

    Added new permission

--- social_media_api/permissions.py
+++ social_media_api/permissions.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
@@ -11,3 +12,10 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
             )
             or (request.user and request.user.is_staff)
         )
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
